401b81a06b23301c513e1681545c7c0a7f7c8adb
xenyeo <yeoyiwen@gmail.com>
2025-01-23 09:25:45 +0800

    Improved skeleton of chatbot. (Added support for echo)

--- src/main/java/Kaji.java
+++ src/main/java/Kaji.java
@@ -1,24 +1,41 @@
+import java.util.Scanner;
+
 public class Kaji {
+    private static final String INDENTATION = "    ";
+    private static final String SEPARATOR = INDENTATION + "____________________________________________________________";
+    // Design adapted from https://patorjk.com/software/taag
+    private static final String LOGO = INDENTATION
+            + "    )               (\n"
+            + "      ( /(   (           )\\ )\n"
+            + "      )\\())  )\\      (  (()/(\n"
+            + "     ((_)\\((((_)(    )\\  /(_))\n"
+            + "     _ ((_))\\ _ )\\  ((_)(_))\n"
+            + "    | |/ / (_) \\(_)_ | ||_ _|\n"
+            + "    | ' (   / _ \\ | || | | |\n"
+            + "    |_|\\_\\ /_/ \\_\\ \\__/ |___|";
+    private static final String GREET = INDENTATION + "Hello! I'm KAJI, your personal virtual assistant.\n"
+            + INDENTATION + "How can i help you today?";
+    private static final String EXIT = INDENTATION + "Thank you for chatting with me!\n"
+            + INDENTATION + "See you again soon!";
+
     public static void main(String[] args) {
-        // Design adapted from https://patorjk.com/software/taag
-        String logo = "    )               (\n"
-                + "  ( /(   (           )\\ )\n"
-                + "  )\\())  )\\      (  (()/(\n"
-                + " ((_)\\((((_)(    )\\  /(_))\n"
-                + " _ ((_))\\ _ )\\  ((_)(_))\n"
-                + "| |/ / (_) \\(_)_ | ||_ _|\n"
-                + "| ' (   / _ \\ | || | | |\n"
-                + "|_|\\_\\ /_/ \\_\\ \\__/ |___|";
-        String separator = "____________________________________________________________";
-        String greet = "Hello! I'm KAJI, your personal virtual assistant.\n"
-                + "How can i help you today?";
-        String exit = "Thank you for chatting with me!\n"
-                + "See you again soon!";
-        System.out.println(logo);
-        System.out.println(separator);
-        System.out.println(greet);
-        System.out.println(separator);
-        System.out.println(exit);
-        System.out.println(separator);
+        System.out.println(LOGO);
+        System.out.println(SEPARATOR);
+        System.out.println(GREET);
+        System.out.println(SEPARATOR);
+
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = sc.nextLine();
+            System.out.println(SEPARATOR);
+            if (command.equals("bye")) {
+                System.out.println(EXIT);
+            } else {
+                System.out.println(INDENTATION + command);
+            }
+            System.out.println(SEPARATOR);
+        }
+        sc.close();
     }
 }
