5d4241a32ec7a5de12462de6c316cd6aae211b07
xenyeo <yeoyiwen@gmail.com>
2025-01-24 12:26:19 +0800

    Changed array implementation to ArrayList<Task>

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,11 +1,12 @@
 import Exceptions.TaskIDOutOfBoundException;
 
+import java.util.ArrayList;
+
 /**
  * Represents a simple task list that allows adding and viewing tasks.
  */
 public class TaskList {
-    private final Task[] list = new Task[100];
-    private int taskId = 0;
+    private ArrayList<Task> list = new ArrayList<>();
 
     /**
      * Adds a new task to the task list.
@@ -15,21 +16,20 @@ public class TaskList {
         System.out.println(UI.SEPARATOR);
         System.out.println(UI.INDENTATION + "This task has been added:");
         switch (type) {
-            case "T" -> list[taskId] = new ToDo(description, type);
+            case "T" -> list.add(new ToDo(description, type));
             case "D" -> {
                 String[] descriptionParts = description.split(" /by ");
-                list[taskId] = new Deadline(descriptionParts[0], type, descriptionParts[1]);
+                list.add(new Deadline(descriptionParts[0], type, descriptionParts[1]));
             }
             case "E" -> {
                 String[] descriptionParts = description.split(" /from ");
                 String[] range = descriptionParts[1].split(" /to ");
-                list[taskId] = new Event(descriptionParts[0], type, range[0], range[1]);
+                list.add(new Event(descriptionParts[0], type, range[0], range[1]));
             }
         }
-        System.out.println(UI.INDENTATION + " " + list[taskId].toString());
-        taskId++;
-        String taskWord = (taskId == 1) ? "task" : "tasks";
-        System.out.println(UI.INDENTATION + "There are now " + taskId + " " + taskWord + " in your list.");
+        System.out.println(UI.INDENTATION + " " + list.get(list.size() - 1).toString());
+        String taskWord = (list.size() == 1) ? "task" : "tasks";
+        System.out.println(UI.INDENTATION + "There are now " + list.size() + " " + taskWord + " in your list.");
         System.out.println(UI.SEPARATOR);
     }
 
@@ -39,8 +39,8 @@ public class TaskList {
     public void printTasks() {
         System.out.println(UI.SEPARATOR);
         System.out.println(UI.INDENTATION + "Here are the current tasks in your list:");
-        for (int i = 0; i < taskId; i++) {
-            System.out.println(UI.INDENTATION + (i+1) + ". " + list[i].toString());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(UI.INDENTATION + (i+1) + ". " + list.get(i).toString());
         }
         System.out.println(UI.SEPARATOR);
     }
@@ -52,10 +52,10 @@ public class TaskList {
      */
     public void markTask(int taskId) {
         try {
-            if (taskId > this.taskId || taskId == 0) {
+            if (taskId > list.size() || taskId == 0) {
                 throw new TaskIDOutOfBoundException("taskId out of bound");
             } else {
-                Task currentTask = list[taskId - 1];
+                Task currentTask = list.get(list.size() - 1);
                 currentTask.markTask();
                 System.out.println(UI.SEPARATOR);
                 System.out.println(UI.INDENTATION + "Well Done! This task is now done:");
@@ -63,7 +63,7 @@ public class TaskList {
                 System.out.println(UI.SEPARATOR);
             }
         } catch (TaskIDOutOfBoundException e) {
-            String range = this.taskId == 0 ? "nil" : "1-" + taskId;
+            String range = list.isEmpty() ? "nil" : "1-" + list.size();
             System.out.println(UI.SEPARATOR);
             System.out.println(UI.INDENTATION + "Task number is out of range.\n"
                     + UI.INDENTATION + "Current range: " + range + " (Given: " + taskId + ")\n"
@@ -79,10 +79,10 @@ public class TaskList {
      */
     public void unmarkTask(int taskId) {
         try {
-            if (taskId > this.taskId || taskId == 0) {
+            if (taskId > list.size() || taskId == 0) {
                 throw new TaskIDOutOfBoundException("taskId out of bound");
             } else {
-                Task currentTask = list[taskId - 1];
+                Task currentTask = list.get(list.size() - 1);
                 currentTask.unmarkTask();
                 System.out.println(UI.SEPARATOR);
                 System.out.println(UI.INDENTATION + "Well Done! This task is now done:");
@@ -90,7 +90,7 @@ public class TaskList {
                 System.out.println(UI.SEPARATOR);
             }
         } catch (TaskIDOutOfBoundException e) {
-            String range = this.taskId == 0 ? "nil" : "1-" + taskId;
+            String range = list.isEmpty() ? "nil" : "1-" + list.size();
             System.out.println(UI.SEPARATOR);
             System.out.println(UI.INDENTATION + "Task number is out of range.\n"
                     + UI.INDENTATION + "Current range: " + range + " (Given: " + taskId + ")\n"
