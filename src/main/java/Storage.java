2b862336fa7ffd138abe7197dcdab8300dd1c4e7
xenyeo <yeoyiwen@gmail.com>
2025-01-29 14:59:02 +0800

    Add boilerplate code to Storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+/**
+ * Handles the storage functionalities for the KAJI virtual assistant.
+ */
+public class Storage {
+    private static final String FILE_PATH = "./data/kaji.txt";
+
+    public void loadTasks(TaskList taskList) {
+
+    }
+
+    public void saveTasks(TaskList taskList) {
+
+    }
+}
