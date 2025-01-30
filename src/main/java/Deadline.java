f58544510ee1337ce0f00fb56c7f2ee42e94e1a7
xenyeo <yeoyiwen@gmail.com>
2025-01-30 16:13:03 +0800

    Update task child classes to OOP format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,22 @@
-import Exceptions.InvalidDateException;
-
 public class Deadline extends Task {
     protected String by;
 
-    public Deadline(String type, Boolean isDone, String description, String by) throws InvalidDateException {
+    public Deadline(String type, Boolean isDone, String description, String by) {
         super(type, isDone, description);
-        String validPattern = "\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4}";
-        if (by.matches(validPattern)) {
-            this.by = by;
-        } else {
-            throw new InvalidDateException("Invalid date format");
-        }
+        this.by = by;
     }
+//    public Deadline(String type, Boolean isDone, String description, String by) throws InvalidDateException {
+//        super(type, isDone, description);
+//        String validPattern = "\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4}";
+//        if (by.matches(validPattern)) {
+//            this.by = by;
+//        } else {
+//            throw new InvalidDateException("Invalid date format");
+//        }
+//    }
 
     @Override
     public String toString() {
-        return super.toString() + " (by: " + TaskList.convertDateTimeFormat(by) + ")";
+        return super.toString() + " (by: " + by + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,41 +1,47 @@
-import Exceptions.InvalidDateException;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     protected String start;
     protected String end;
 
-    public Event(String type, Boolean isDone, String description, String start, String end)
-            throws InvalidDateException {
+    public Event(String type, Boolean isDone, String description, String start, String end) {
         super(type, isDone, description);
-        String validPattern = "\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4}";
-        if (start.matches(validPattern) && end.matches(validPattern)) {
-            this.start = start;
-            this.end = end;
-        } else {
-            throw new InvalidDateException("Invalid date format");
-        }
+        this.start = start;
+        this.end = end;
     }
 
-    /**
-     * Checks if the start and end date strings represent the same calendar date.
-     *
-     * @param start The start date and time in the format "d/M/yyyy HHmm".
-     * @param end The end date and time in the format "d/M/yyyy HHmm".
-     * @return true if the start and end dates are the same calendar date, false otherwise.
-     */
-    private boolean isSameDate(String start, String end) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-        LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
-        LocalDateTime endDateTime = LocalDateTime.parse(end, formatter);
-        return startDateTime.toLocalDate().equals(endDateTime.toLocalDate());
-    }
+//    public Event(String type, Boolean isDone, String description, String start, String end)
+//            throws InvalidDateException {
+//        super(type, isDone, description);
+//        String validPattern = "\\d{1,2}/\\d{1,2}/\\d{4}\\s+\\d{4}";
+//        if (start.matches(validPattern) && end.matches(validPattern)) {
+//            this.start = start;
+//            this.end = end;
+//        } else {
+//            throw new InvalidDateException("Invalid date format");
+//        }
+//    }
+//
+//    /**
+//     * Checks if the start and end date strings represent the same calendar date.
+//     *
+//     * @param start The start date and time in the format "d/M/yyyy HHmm".
+//     * @param end The end date and time in the format "d/M/yyyy HHmm".
+//     * @return true if the start and end dates are the same calendar date, false otherwise.
+//     */
+//    private boolean isSameDate(String start, String end) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+//        LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
+//        LocalDateTime endDateTime = LocalDateTime.parse(end, formatter);
+//        return startDateTime.toLocalDate().equals(endDateTime.toLocalDate());
+//    }
 
+//    @Override
+//    public String toString() {
+//        return super.toString() + " (from: " + TaskList.convertDateTimeFormat(start) + " to: "
+//                + (isSameDate(start, end) ? end.split(" ")[1] : TaskList.convertDateTimeFormat(end)) + ")";
+//    }
     @Override
     public String toString() {
-        return super.toString() + " (from: " + TaskList.convertDateTimeFormat(start) + " to: "
-                + (isSameDate(start, end) ? end.split(" ")[1] : TaskList.convertDateTimeFormat(end)) + ")";
+        return super.toString() + " (from: " + start + " to: " + end + ")";
     }
 }
